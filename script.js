f4095f9dd74f9f609afc91617f0089d772e7269e
guilherme <guilhermepolicarpio@gmail.com>
2022-06-26 22:34:43 -0300

    Ajustes tela inicial e layout

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 var nome=undefined; //nome do usuário
 var envioNome=undefined; //envio do objeto
 
-//Função que solicita o nome do usuário e envia para o servidor
-function entrada(){
+function enviarNome(){
 
-    nome = prompt("Insira seu nome (máximo 10 caracteres):");
- 
-    for(let i=0;i<nome.length;i++){
-        if(i>9)
-            {
-            alert(`O nome inserido tem mais de 10 caracteres. Por gentileza, inserir outro nome.`);
-            i=nome.length;
-            entrada();
-            }
-    }
+    nome = document.querySelector("input").value;
 
     envioNome={
         name:`${nome}`
@@ -21,25 +11,22 @@ function entrada(){
 
     //Envio de requisição com nome do usuário para o servidor
     let promise=axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", envioNome)
-
-    //Requisição validada com sucesso
     promise.then(sucesso);
-    //Requisição com erro
     promise.catch(erro);
 }
 
 //Função para mostrar no console que a requisição foi enviada e validada com sucesso
 function sucesso(){
-    if(sucesso.status === 200){
-        console.log("deu bom");
-       }
+    var tela = document.querySelector(".telaInicial");
+    tela.classList.add("hidden");
+    var tela2 = document.querySelector(".telaChat");
+    tela2.classList.remove('hidden');
     }
 
 //Função para identificação e tratamento de erros
 function erro(error){
     alert("Já existe um usuário com este nome!");
-    console.log(error.response.status);
-    entrada();
+    console.log("Erro: "+ error.response.status);
 }
 
 //Função que envia uma requisição para o servidor em conjunto com a função setInterval a cada 5s para manter o usuario online
@@ -64,7 +51,7 @@ function exibirMensagens(sucesso){
         if(sucesso.data[i].type ==="status"){
         adicionarMensagem += 
         `<div class = "status mensagem" data-identifier="message">
-             <span class="horario">  ${sucesso.data[i].time}     </span>
+             <span class="horario">  (${sucesso.data[i].time})     </span>
              <span class="de">    ${sucesso.data[i].from}      </span>
              <span class="texto">    ${sucesso.data[i].text}      </span>
         </div>`
@@ -72,10 +59,10 @@ function exibirMensagens(sucesso){
         else if(sucesso.data[i].type==="message"){
             adicionarMensagem += 
             `<div class = "message mensagem">
-                 <span class="horario">  ${sucesso.data[i].time}     </span>
+                 <span class="horario">  (${sucesso.data[i].time})    </span>
                  <span class="de">    ${sucesso.data[i].from}      </span>
                  para
-                 <span class="de">    ${sucesso.data[i].to}      </span>
+                 <span class="de">    ${sucesso.data[i].to}:    </span>
                  <span class="texto">    ${sucesso.data[i].text}      </span>
             </div>`
         }
@@ -83,10 +70,10 @@ function exibirMensagens(sucesso){
         else if(sucesso.data[i].type==="private_message"){
             adicionarMensagem += 
             `<div class = "private_message mensagem">
-                 <span class="horario">  ${sucesso.data[i].time}     </span>
+                 <span class="horario">  (${sucesso.data[i].time})     </span>
                  <span class="de">    ${sucesso.data[i].from}      </span>
                  reservadamente para
-                 <span class="tp">    ${sucesso.data[i].to}      </span>
+                 <span class="tp">    ${sucesso.data[i].to}:      </span>
                  <span class="texto">    ${sucesso.data[i].text}      </span>
             </div>`
         }
@@ -100,7 +87,7 @@ function exibirMensagens(sucesso){
 }
 
 function enviarMensagem(){
-    const mensagem = document.querySelector("input").value;
+    const mensagem = document.querySelector("#campo").value;
 
     const objetoMensagem ={
         from: nome,
@@ -120,7 +107,7 @@ function limparCampo(){
     document.getElementById('campo').value='';
 }
 //Chama a função que pergunta e envia o nome de usuário para o servidor
-entrada();
+//entrada();
 
 //Chama a função que avisa o servidor a cada 5s que o usuário está online
 setInterval(manterOnline,5000);
